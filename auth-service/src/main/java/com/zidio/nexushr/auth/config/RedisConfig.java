6837e4138835d3afe512f8adbe0be0c6f27dd358
Mohit Patil <patilmohit978@gmail.com>
2026-06-29 10:50:09 +0530

    fix: disable Redis for cloud deployment

--- auth-service/src/main/java/com/zidio/nexushr/auth/config/RedisConfig.java
+++ auth-service/src/main/java/com/zidio/nexushr/auth/config/RedisConfig.java
@@ -1,15 +1,14 @@
 package com.zidio.nexushr.auth.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-
 
 @Configuration
-@ConditionalOnProperty(name = "spring.data.redis.enabled", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(name = "redis.enabled", havingValue = "true", matchIfMissing = false)
 public class RedisConfig {
 
     @Bean
